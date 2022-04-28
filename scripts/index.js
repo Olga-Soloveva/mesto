5357d45a49d772d8a352d7aa6d4e74b3bd674051
Olga Soloveva <olga.tovstaya@yandex.ru>
2022-04-28 17:27:03 +0300

    feat: добавить скрипт js

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,30 @@
+let popup = document.querySelector('.popup');
 
+function editButtonClick() {
+  popup.classList.toggle('popup_opened');
+}
+
+let editButton = document.querySelector('.edit-button');
+editButton.addEventListener('click', editButtonClick);
+
+let popupClose = document.querySelector('.popup__close');
+popupClose.addEventListener('click', editButtonClick);
+
+let popupForm = document.querySelector('.popup__form')
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  let formName = document.querySelector('.popup__form-item_el_name');
+  let formDescription = document.querySelector('.popup__form-item_el_description');
+  let formNameValue = formName.value;
+  let formDescriptionValue = formDescription.value;
+
+  let profilePersonName = document.querySelector('.profile__person-name');
+  let profileDescription = document.querySelector('.profile__description');
+  profilePersonName.textContent = formNameValue;
+  profileDescription.textContent = formDescriptionValue;
+
+  editButtonClick()
+}
+
+popupForm.addEventListener('submit', formSubmitHandler);
