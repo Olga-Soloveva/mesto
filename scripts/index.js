2ac510759fa142480141f666c975ebcbb1e147a4
Olga Soloveva <olga.tovstaya@yandex.ru>
2022-04-29 12:51:03 +0300

    fix: исправление замечаний после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,32 @@
 let popup = document.querySelector('.popup');
 
-function editButtonClick() {
+function togglePopup() {
   popup.classList.toggle('popup_opened');
 }
 
-let editButton = document.querySelector('.edit-button');
-editButton.addEventListener('click', editButtonClick);
+let formName = document.querySelector('.popup__form-item_el_name');
+let formDescription = document.querySelector('.popup__form-item_el_description');
+let profilePersonName = document.querySelector('.profile__person-name');
+let profileDescription = document.querySelector('.profile__description');
 
-let popupClose = document.querySelector('.popup__close');
-popupClose.addEventListener('click', editButtonClick);
-
-let popupForm = document.querySelector('.popup__form')
+function openForm() {
+  formName.value = profilePersonName.textContent
+  formDescription.value =  profileDescription.textContent
+  togglePopup()
+}
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  let formName = document.querySelector('.popup__form-item_el_name');
-  let formDescription = document.querySelector('.popup__form-item_el_description');
-  let formNameValue = formName.value;
-  let formDescriptionValue = formDescription.value;
-
-  let profilePersonName = document.querySelector('.profile__person-name');
-  let profileDescription = document.querySelector('.profile__description');
-  profilePersonName.textContent = formNameValue;
-  profileDescription.textContent = formDescriptionValue;
-
-  editButtonClick()
+  profilePersonName.textContent = formName.value;
+  profileDescription.textContent = formDescription.value;
+  togglePopup()
 }
 
+let popupClose = document.querySelector('.popup__close');
+popupClose.addEventListener('click', togglePopup);
+
+let editButton = document.querySelector('.edit-button');
+editButton.addEventListener('click', openForm);
+
+let popupForm = document.querySelector('.popup__form')
 popupForm.addEventListener('submit', formSubmitHandler);
